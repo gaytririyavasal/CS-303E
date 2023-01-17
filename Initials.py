9cd7bc3ede84abc85c929c4f540538ec9ed90f30
gaytririyavasal <99584784+gaytririyavasal@users.noreply.github.com>
2023-01-17 00:38:26 -0600

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,21 @@
+# File: Initials.py
+# Student: Gaytri Riya Vasal
+# UT EID: grv377
+# Course: CS303E
+# 
+# Date Created: 9/1/2021
+# Date Last Modified: 9/3/2021
+# Description of Program: The code below contains my three initials.
+
+print()
+print("  GGGGGGGGGGGG      RRRRRRRRRRRR      VV        VV      ")
+print("  GGGGGGGGGGGG      RRRRRRRRRRRR      VV        VV      ")
+print("  GG                RR        RR      VV        VV      ")
+print("  GG                RRRRRRRRRRRR      VV        VV      ")
+print("  GG    GGGGGG      RRRRRRRRRRRR      VV        VV      ")
+print("  GG    GGGGGG      RRRR              VV        VV      ")
+print("  GG        GG      RR  RR            VV        VV      ")
+print("  GG        GG      RR    RR          VVV      VVV      ")
+print("  GGGGGGGGGGGG  ..  RR      RR    ..    VVVVVVVV    ..  ")
+print("  GGGGGGGGGGGG  ..  RR        RR  ..     VVVVVV     ..  ")
+print()
